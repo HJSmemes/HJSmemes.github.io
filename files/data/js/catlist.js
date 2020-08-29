e1aa0ba49c7d7cad40c9e4974adf267d351daa87
3EGaming <3EGaming@users.noreply.github.com>
2020-08-29 13:32:37 +0100

    Update catlist.js

--- files/data/js/catlist.js
+++ files/data/js/catlist.js
@@ -1,18 +1,18 @@
 function readCat() {
 var resA = getFileAsString("./files/memes/games/MEMELIST.txt")
-var gamecatlistT = resultx.split("\n");
+var gamecatlistT = window.resultx.split("\n");
 window.gamecatlist = [];
 for(memename in gamecatlistT) {
   window.gamecatlist.push("games/".concat(memename))
 }
 var resB = getFileAsString("./files/memes/general/MEMELIST.txt")
-var gencatlistT = resultx.split("\n");
+var gencatlistT = window.resultx.split("\n");
 window.gencatlist = [];
 for(memename in gencatlistT) {
   window.gencatlist.push("genaral/".concat(memename))
 }
 var resC = getFileAsString("./files/memes/tv/MEMELIST.txt")
-var tvcatlistT = resultx.split("\n");
+var tvcatlistT = window.resultx.split("\n");
 window.tvcatlist = [];
 for(memename in tvcatlistT) {
   window.tvcatlist.push("tv/".concat(memename))
